cf65958bfdfea1abb90300457d47f2435deb80f5
Caner Polat <caner.polat@ipt.ch>
2024-01-09 16:24:22 +0100

    Final scene added

--- src/main.cc
+++ src/main.cc
@@ -415,7 +415,7 @@ void cornell_smoke() {
     cam.render(world);
 }
 
-void mesh_scene() {
+void mesh_scene_nefertiti() {
     hittable_list world;
 
     auto difflight = make_shared<diffuse_light>(color(4,4,4));
@@ -460,7 +460,7 @@ void mesh_scene() {
     cam.display(world);
 }
 
-void mesh_scene_2() {
+void mesh_scene_dragon() {
     hittable_list world;
 
     auto difflight = make_shared<diffuse_light>(color(4,4,4));
@@ -479,7 +479,7 @@ void mesh_scene_2() {
 
     mesh_loader loader = mesh_loader();
     mesh dragon_mesh;
-    loader.load("./mesh/Nefertiti.obj", dragon_mesh);
+    loader.load("./mesh/xyzrgb_dragon.obj", dragon_mesh);
     hittable_list dragon_obj;
     dragon_mesh.create_object(dragon_obj, left_red, 1);
     world.add(make_shared<hittable_list>(dragon_obj));
@@ -504,8 +504,89 @@ void mesh_scene_2() {
     cam.display(world);
 }
 
+void final_scene(int image_width, int samples_per_pixel, int max_depth) {
+    hittable_list boxes1;
+    auto ground = make_shared<lambertian>(color(0.48, 0.83, 0.53));
+
+    int boxes_per_side = 20;
+    for (int i = 0; i < boxes_per_side; i++) {
+        for (int j = 0; j < boxes_per_side; j++) {
+            auto w = 100.0;
+            auto x0 = -1000.0 + i*w;
+            auto z0 = -1000.0 + j*w;
+            auto y0 = 0.0;
+            auto x1 = x0 + w;
+            auto y1 = random_double(1,101);
+            auto z1 = z0 + w;
+
+            boxes1.add(box(point3(x0,y0,z0), point3(x1,y1,z1), ground));
+        }
+    }
+
+    hittable_list world;
+
+    world.add(make_shared<bvh_node>(boxes1));
+
+    auto light = make_shared<diffuse_light>(color(7, 7, 7));
+    world.add(make_shared<quad>(point3(123,554,147), vec3(300,0,0), vec3(0,0,265), light));
+
+    auto center1 = point3(400, 400, 200);
+    auto center2 = center1 + vec3(30,0,0);
+    auto sphere_material = make_shared<lambertian>(color(0.7, 0.3, 0.1));
+    world.add(make_shared<sphere>(center1, center2, 50, sphere_material));
+
+    world.add(make_shared<sphere>(point3(260, 150, 45), 50, make_shared<dielectric>(1.5)));
+    world.add(make_shared<sphere>(
+        point3(0, 150, 145), 50, make_shared<metal>(color(0.8, 0.8, 0.9), 1.0)
+    ));
+
+    auto boundary = make_shared<sphere>(point3(360,150,145), 70, make_shared<dielectric>(1.5));
+    world.add(boundary);
+    world.add(make_shared<constant_medium>(boundary, 0.2, color(0.2, 0.4, 0.9)));
+    boundary = make_shared<sphere>(point3(0,0,0), 5000, make_shared<dielectric>(1.5));
+    world.add(make_shared<constant_medium>(boundary, .0001, color(1,1,1)));
+
+    auto emat = make_shared<lambertian>(make_shared<image_texture>("earthmap.jpg"));
+    world.add(make_shared<sphere>(point3(400,200,400), 100, emat));
+    // auto pertext = make_shared<noise_texture>(0.1);
+    auto pertext = make_shared<noise_texture>();
+    world.add(make_shared<sphere>(point3(220,280,300), 80, make_shared<lambertian>(pertext)));
+
+    hittable_list boxes2;
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    int ns = 1000;
+    for (int j = 0; j < ns; j++) {
+        boxes2.add(make_shared<sphere>(point3::random(0,165), 10, white));
+    }
+
+    world.add(make_shared<translate>(
+        make_shared<rotate_y>(
+            make_shared<bvh_node>(boxes2), 15),
+            vec3(-100,270,395)
+        )
+    );
+
+    camera cam;
+
+    cam.aspect_ratio      = 1.0;
+    cam.image_width       = image_width;
+    cam.samples_per_pixel = samples_per_pixel;
+    cam.max_depth         = max_depth;
+    cam.background        = color(0,0,0);
+
+    cam.vfov     = 40;
+    cam.lookfrom = point3(478, 278, -600);
+    cam.lookat   = point3(278, 278, 0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+    // cam.display(world);
+}
+
 int main() { // Easy switch between scenes
-    switch (2)
+    switch (13)
     {
     case 1:
         main_schene();
@@ -538,10 +619,13 @@ int main() { // Easy switch between scenes
         triangles();
         break;
     case 11:
-        mesh_scene();
+        mesh_scene_nefertiti();
         break;
     case 12:
-        mesh_scene_2();
+        mesh_scene_dragon();
+        break;
+    case 13:
+        final_scene(800, 10, 20);
         break;
     default:
         break;
